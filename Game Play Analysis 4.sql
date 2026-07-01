ad888be4ae499f001def23c26563beb82ed83b2f
Ramyakannan <raajikannan1105@gmail.com>
2026-07-01 22:14:49 +0530

    SQL

---
+++ Game Play Analysis 4.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+SELECT 
+    ROUND(
+        COUNT(DISTINCT next_day.player_id) / COUNT(DISTINCT first_login.player_id),
+        2
+    ) AS fraction
+FROM (
+    SELECT player_id, MIN(event_date) AS first_login
+    FROM Activity
+    GROUP BY player_id
+) first_login
+LEFT JOIN Activity next_day
+ON first_login.player_id = next_day.player_id
+AND next_day.event_date = DATE_ADD(first_login.first_login, INTERVAL 1 DAY);
